5beaa7245f120328ab48b46046395033a55642d6
root <root@gzyw53-dev-liyunxuan-01.snail>
2020-04-01 15:16:59 +0800

    基本投票功能，自定义模板

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Question
 from django.http import Http404
+from django.urls import reverse
 
 
 # Create your views here
@@ -29,8 +30,23 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html',{'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # request.POST 可通过关键字名获取提交数据，此处以字符串享实返回选择的 Choice id
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 如果 choice 不存在则引发 keyerror，跳转到问题 detail 页面并告知错误
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        # 投票后，投票值加1
+        selected_choice.votes += 1
+        selected_choice.save()
+        # 投票完成后将用户重定向到结果，防止用户点击两次按钮
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
